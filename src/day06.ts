39ae4013f81f9a4cc24391f68a7081bd9082493d
Paulus Esterhazy <pesterhazy@gmail.com>
2021-12-07 07:38:08 +0100

    WIP

--- src/day06.ts
+++ src/day06.ts
@@ -3,16 +3,14 @@ export function calc(days: number) {
 
   for (let day = 0; day < days; day++) {
     let newGen = [];
-    let born = 0;
     for (let g of gen) {
       if (g === 0) {
         newGen.push(6);
-        born++;
+        newGen.push(8);
       } else {
         newGen.push(g - 1);
       }
     }
-    for (let i = 0; i < born; i++) newGen.push(8);
     gen = newGen;
   }
   return gen.length;
