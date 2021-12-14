55f81e7c01650799cb709b2c865fe7e28e5c1b1f
Paulus Esterhazy <pesterhazy@gmail.com>
2021-12-14 22:56:00 +0100

    WIP

--- src/day14.test.ts
+++ src/day14.test.ts
@@ -47,3 +47,10 @@ test("should give answer", () => {
   let r = day.solvea(p);
   expect(r).toBe(2745);
 });
+
+test("should f", () => {
+  let inp = demo;
+  let p = day.parse(inp);
+  expect(day.f("NN", 1, p.dict)).toStrictEqual({ N: 2, C: 1 });
+  expect(day.f("NN", 2, p.dict)).toStrictEqual({ N: 2, C: 2, B: 1 });
+});
--- src/day14.ts
+++ src/day14.ts
@@ -44,3 +44,42 @@ export function solvea(input: Input): number {
   xs.sort((a, b) => a - b);
   return xs[xs.length - 1] - xs[0];
 }
+
+function add(
+  a: Record<string, number>,
+  b: Record<string, number>
+): Record<string, number> {
+  let r = { ...a };
+
+  for (let [k, v] of Object.entries(b)) {
+    r[k] = (r[k] || 0) + v;
+  }
+  return r;
+}
+
+export function f(
+  pair: string,
+  n: number,
+  dict: Record<string, string>
+): Record<string, number> {
+  let result;
+  if (n === 1) {
+    let r: Record<string, number> = {};
+
+    r[pair[0]] = (r[pair[0]] || 0) + 1;
+    r[pair[1]] = (r[pair[1]] || 0) + 1;
+    r[dict[pair]] = (r[dict[pair]] || 0) + 1;
+    result = r;
+  } else {
+    let cc: Record<string, number> = {};
+    cc[dict[pair]] = -1;
+    result = add(
+      add(
+        f(pair[0] + dict[pair], n - 1, dict),
+        f(dict[pair] + pair[1], n - 1, dict)
+      ),
+      cc
+    );
+  }
+  return result;
+}
