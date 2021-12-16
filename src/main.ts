4ce7e30e2fc21c6e163a0d6b488c4cb6b4216dfa
Paulus Esterhazy <pesterhazy@gmail.com>
2021-12-16 18:57:59 +0100

    WIP

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,8 @@ const demo = `1163751742
 1293138521
 2311944581`;
 
+// guess: 3372 is too high
+
 let result = day.solvea(day.expand(day.parse(util.slurp("data/day15.txt"))));
 // let result = day.solvea(day.expand(day.parse(demo)));
 
