fd8981018483f1905daf0888e8d22563710ed3e7
Paulus Esterhazy <pesterhazy@gmail.com>
2021-12-16 23:41:17 +0100

    WIP

--- src/day16.test.ts
+++ src/day16.test.ts
@@ -5,18 +5,23 @@ test("should parse literal", () => {
   let packet = day.fromString("D2FE28").readPacket();
   if (!day.isLiteral(packet)) throw "Not literal";
   expect(packet.version).toBe(6);
-  expect(packet.typeID).toBe(day.PacketType.Literal);
+  expect(packet.ptype).toBe(day.PacketType.Literal);
   expect(packet.v).toBe(2021);
 });
 
 test("should parse operator", () => {
   let packet = day.fromString("38006F45291200").readPacket();
   expect(packet.version).toBe(1);
-  expect(packet.typeID).toBe(day.PacketType.OperatorA);
+  expect(packet.ptype).toBe(day.PacketType.Operator);
 });
 
 test("should parse another operator", () => {
   let packet = day.fromString("EE00D40C823060").readPacket();
   expect(packet.version).toBe(7);
-  expect(packet.typeID).toBe(day.PacketType.OperatorB);
+  expect(packet.ptype).toBe(day.PacketType.Operator);
+});
+
+test("should answer demo", () => {
+  let n = day.solvea(day.fromString(util.slurp("data/day16.txt")));
+  expect(n).toBe(-999);
 });
--- src/day16.ts
+++ src/day16.ts
@@ -1,30 +1,26 @@
 export enum PacketType {
-  Literal = 4,
-  OperatorA = 6,
-  OperatorB = 3
+  Literal,
+  Operator
 }
 
 interface LiteralPacket {
   version: number;
-  typeID: PacketType.Literal;
+  typeID: number;
+  ptype: PacketType.Literal;
   v: number;
 }
 
 export function isLiteral(p: Packet): p is LiteralPacket {
-  return p.typeID === PacketType.Literal;
+  return p.ptype === PacketType.Literal;
 }
 
-interface OperatorAPacket {
+interface OperatorPacket {
   version: number;
-  typeID: PacketType.OperatorA;
+  typeID: number;
+  ptype: PacketType.Operator;
 }
 
-interface OperatorBPacket {
-  version: number;
-  typeID: PacketType.OperatorB;
-}
-
-type Packet = LiteralPacket | OperatorAPacket | OperatorBPacket;
+type Packet = LiteralPacket | OperatorPacket;
 
 class Reader {
   s: string;
@@ -33,6 +29,10 @@ class Reader {
   constructor(s: string) {
     this.s = s;
     this.pos = 0;
+  }
+
+  eof(): boolean {
+    return this.pos >= this.s.length;
   }
 
   readBits(n: number): string {
@@ -55,7 +55,7 @@ class Reader {
     let version = this.readBitsAsNumber(3);
     let typeID = this.readBitsAsNumber(3);
 
-    if (typeID === PacketType.Literal) {
+    if (typeID === 4) {
       let bits: string = "";
       while (this.readBitsAsNumber(1) === 1) {
         bits += this.readBits(4);
@@ -63,11 +63,8 @@ class Reader {
       bits += this.readBits(4);
       let v = parseInt(bits, 2);
 
-      return { version, typeID, v };
-    } else if (
-      typeID === PacketType.OperatorA ||
-      typeID === PacketType.OperatorB
-    ) {
+      return { ptype: PacketType.Literal, version, typeID, v };
+    } else {
       let lengthTypeID = this.readBitsAsNumber(1);
 
       if (lengthTypeID === 0) {
@@ -80,8 +77,8 @@ class Reader {
         }
       } else throw "Unexpected lengthTypeID";
 
-      return { version, typeID };
-    } else throw "Unexpected typeID";
+      return { ptype: PacketType.Operator, version, typeID };
+    }
   }
 }
 
@@ -93,4 +90,13 @@ export function fromString(s: string): Reader {
       .padStart(4, "0");
   }
   return new Reader(ss);
+}
+
+export function solvea(reader: Reader): number {
+  let ans = 0;
+  while (!reader.eof()) {
+    let packet = reader.readPacket();
+    ans += packet.version;
+  }
+  return ans;
 }
