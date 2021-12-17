1c7c1b952d3e0419eff88948147aeb2f718d41f9
Paulus Esterhazy <pesterhazy@gmail.com>
2021-12-17 18:17:39 +0100

    WIP

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   setupFilesAfterEnv: [
     '<rootDir>support/check-assertions-number.js',
   ],
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
   "testPathIgnorePatterns": [
     "\\/\\.#"
   ],
