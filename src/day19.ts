d42bdd8a6890a9834abd390c9233c50254b729d1
Paulus Esterhazy <pesterhazy@gmail.com>
2021-12-20 14:41:33 +0100

    WIP

---
+++ src/day19.ts
@@ -0,0 +1,20 @@
+type Vec3 = [number, number, number];
+type Scanner = Vec3[];
+
+export function parse(s: string): Scanner[] {
+  let lines = s.split(/\n/);
+  let n = -1;
+  let r: Scanner[] = [];
+  for (let line of lines) {
+    if (line.startsWith("---")) {
+      n++;
+      r[n] = [];
+    } else if (line.length === 0) continue;
+    else {
+      let m = line.match(/([-0-9]+)/g);
+      if (!m) throw "oops";
+      r[n].push(m.map(ss => parseInt(ss)) as Vec3);
+    }
+  }
+  return r;
+}
