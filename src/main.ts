bb75c10e9b6f6b4c5ef4f3993b6869cf527c8ac7
Paulus Esterhazy <pesterhazy@gmail.com>
2021-12-21 18:11:12 +0100

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import * as day from "./day21";
 import * as util from "./util";
 
-// let result = day.solveb(day.parse(util.slurp("data/day21.txt")));
-// expected: 444356092776315
-// actual:   37107702442602
-let result = day.solveb(
-  day.parse(`Player 1 starting position: 4
-Player 2 starting position: 8`)
-);
+let result = day.solveb(day.parse(util.slurp("data/day21.txt")));
+// let result = day.solveb(
+//   day.parse(`Player 1 starting position: 4
+// Player 2 starting position: 8`)
+// );
 
 console.log(result);
